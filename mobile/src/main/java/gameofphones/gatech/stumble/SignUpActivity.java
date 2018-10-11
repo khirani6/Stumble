cf5d1fcb04a7bd59c585b19597920a39fd1cccf1
khirani6 <khirani6@gatech.edu>
2018-10-11 13:23:48 -0400

    Adding basic layout of SignUpActivity
    - links to Login Screen after hitting sign up button

--- mobile/src/main/java/gameofphones/gatech/stumble/SignUpActivity.java
+++ mobile/src/main/java/gameofphones/gatech/stumble/SignUpActivity.java
@@ -7,34 +7,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     private EditText emailField;
     private EditText passwordField;
-    private Button mLoginButton;
+    private EditText firstNameField;
+    private EditText lastNameField;
     private Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
         emailField = findViewById(R.id.email_field);
         passwordField = findViewById(R.id.password_field);
-        mLoginButton = findViewById(R.id.log_in_button);
+
+
+
         mSignUpButton = findViewById(R.id.sign_up_button);
 
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
                 startActivity(intent);
 
             }
 
         });
-
-
     }
 }
