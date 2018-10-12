35fe6c722ab815d664212f46a93d0ab7086f67ab
clawrence8 <ca.lawrence03@gmail.com>
2018-10-12 00:54:43 -0400

    Added class models for User and EmergencyContact

---
+++ mobile/src/main/java/gameofphones/gatech/stumble/User.java
@@ -0,0 +1,69 @@
+package gameofphones.gatech.stumble;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private String mFirstName;
+    private String mLastName;
+    private String mEmail;
+    private List<EmergencyContact> mEmergencyContacts;
+
+    public User(String firstName, String lastName, String email) {
+        mFirstName = firstName;
+        mLastName = lastName;
+        mEmail = email;
+    }
+
+    public User(String firstName, String lastName, String email,
+                List<EmergencyContact> emergencyContacts) {
+        mFirstName = firstName;
+        mLastName = lastName;
+        mEmail = email;
+        mEmergencyContacts = emergencyContacts;
+    }
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+    public String getLastName() {
+        return mLastName;
+    }
+
+    public void setLastName(String lastName) {
+        mLastName = lastName;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String email) {
+        mEmail = email;
+    }
+
+    public List<EmergencyContact> getEmergencyContacts() {
+        return mEmergencyContacts;
+    }
+
+    public void setEmergencyContacts(List<EmergencyContact> emergencyContacts) {
+        mEmergencyContacts = emergencyContacts;
+    }
+
+    public void addEmergencyContact(EmergencyContact ec) {
+        if (mEmergencyContacts == null) {
+            mEmergencyContacts = new ArrayList<>();
+        }
+        mEmergencyContacts.add(ec);
+    }
+
+    public boolean removeEmergencyContact(EmergencyContact ec) {
+        return mEmergencyContacts.remove(ec);
+    }
+}
