d247a72a838a6472f1fd68326ca58f2d094445f0
khirani6 <khirani6@gatech.edu>
2018-10-13 14:10:15 -0400

    Most of flow is now complete for EC list. Functionality to be able to select a certain EC in detail and edit it not implemented yet (Screen is there though)

--- mobile/src/main/java/gameofphones/gatech/stumble/ECActivity.java
+++ mobile/src/main/java/gameofphones/gatech/stumble/ECActivity.java
@@ -1,17 +1,29 @@
 package gameofphones.gatech.stumble;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ECActivity extends AppCompatActivity {
+
+    private ImageButton mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ec);
+
+        mAddButton = findViewById(R.id.addButton);
+
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ECActivity.this, AddECActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
-
-
-
-
 }
