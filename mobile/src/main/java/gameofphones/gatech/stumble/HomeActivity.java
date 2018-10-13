1a4cf26d2dd13d88710c5acfa711ec7547eb30b4
khirani6 <khirani6@gatech.edu>
2018-10-13 10:45:42 -0400

    Starting emergency contacts page
    - linked between home screen and ec page established
    - changed wording of ec button to be a little more consitent with similar apps with a list of contacts

--- mobile/src/main/java/gameofphones/gatech/stumble/HomeActivity.java
+++ mobile/src/main/java/gameofphones/gatech/stumble/HomeActivity.java
@@ -1,14 +1,36 @@
 package gameofphones.gatech.stumble;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageButton mECButton;
+    private ImageButton mERButton;
+    private ImageButton mAlertButton;
+    private ImageButton mSettingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mECButton = findViewById(R.id.edit_ec_button);
+        mERButton = findViewById(R.id.er_button);
+        mAlertButton = findViewById(R.id.alert_button);
+        mSettingsButton = findViewById(R.id.settings_button);
+
+        mECButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, ECActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
     } //
 
     @Override
