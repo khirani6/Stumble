ed344d7bc4e41e9d909a463ae8b26f8f6e0ae4e3
clawrence8 <ca.lawrence03@gmail.com>
2018-10-13 11:39:34 -0400

    Added network connection abilities

--- mobile/src/main/java/gameofphones/gatech/stumble/MainActivity.java
+++ mobile/src/main/java/gameofphones/gatech/stumble/MainActivity.java
@@ -2,12 +2,17 @@ package gameofphones.gatech.stumble;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -61,5 +66,61 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, LoginTask.Result> {
+        /**
+         * Wrapper class that serves as a union of a result value and an exception. When the download
+         * task has completed, either the result value or exception can be a non-null value.
+         * This allows you to pass exceptions to the UI thread that were thrown during doInBackground().
+         */
+        class Result {
+            public String mResultValue;
+            public Exception mException;
+            public Result(String resultValue) {
+                mResultValue = resultValue;
+            }
+            public Result(Exception exception) {
+                mException = exception;
+            }
+        }
+
+        /**
+         * Defines work to perform on the background thread.
+         */
+        @Override
+        protected LoginTask.Result doInBackground(String... urls) {
+            Result result = null;
+            if (!isCancelled() && urls != null && urls.length > 0) {
+                String urlString = urls[0];
+                try {
+                    URL url = new URL(urlString);
+                    String resultString = NetworkConnection.connect(url);
+                    if (resultString != null) {
+                        result = new Result(resultString);
+                    } else {
+                        throw new IOException("No response received.");
+                    }
+                } catch(Exception e) {
+                    result = new Result(e);
+                }
+            }
+            return result;
+        }
+
+        /**
+         * Updates the UI with the result.
+         */
+        @Override
+        protected void onPostExecute(Result result) {
+            if (result.mResultValue != null) {
+                Log.i("LoginResult", result.mResultValue);
+            }
+
+            if (result.mException != null) {
+                Log.i("LoginResult", result.mException.getMessage());
+            }
+        }
+
     }
 }
