113aad4da8d3166c80a6f122c7e2b5758247c7fe
khirani6 <khirani6@gatech.edu>
2018-10-25 13:08:23 -0400

    Added SMS functionality
    - added some code to handle a phone call, but it's not complete

--- mobile/src/main/java/gameofphones/gatech/stumble/AlertWhoActivity.java
+++ mobile/src/main/java/gameofphones/gatech/stumble/AlertWhoActivity.java
@@ -1,17 +1,44 @@
 package gameofphones.gatech.stumble;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import static android.Manifest.permission.SEND_SMS;
 
 public class AlertWhoActivity extends AppCompatActivity {
 
     String[] templateNamesArr = {"Komal Hirani", "Emergency Room"};
 
     final String alertString = "This is a test alert sent by User";
+
+    private BroadcastReceiver sentStatusReceiver, deliveredStatusReceiver;
+
+    private TelephonyManager mTelephonyManager;
+    private String phoneNumber;
+
+    private static final int REQUEST_SMS = 0;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
+
 
     ListView listView;
 
@@ -24,17 +51,214 @@ public class AlertWhoActivity extends AppCompatActivity {
         listView = findViewById(R.id.alert_who_list);
         listView.setAdapter(alertWho);
 
+        //send a phone call (tutorial: https://google-developer-training.gitbooks.io/android-developer-phone-sms-course/content/Lesson%201/1_c_phone_calls.html)
+        mTelephonyManager = (TelephonyManager)
+                getSystemService(TELEPHONY_SERVICE);
+
+        phoneNumber = String.format("tel: %s",
+                "8322473858");
+
+        if (isTelephonyEnabled()) {
+            checkForPhonePermission();
+            // Log.d(TAG, getString(R.string.telephony_enabled));
+            // Todo: Register the PhoneStateListener.
+            // Todo: Check for permission here.
+        } else {
+            Toast.makeText(this,
+                    "Telephony not enabled",
+                    Toast.LENGTH_LONG).show();
+            //Log.d(TAG, getString(R.string.telephony_not_enabled));
+
+        }
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    int hasSMSPermission = checkSelfPermission(Manifest.permission.SEND_SMS);
+                    if (hasSMSPermission != PackageManager.PERMISSION_GRANTED) {
+                        if (!shouldShowRequestPermissionRationale(Manifest.permission.SEND_SMS)) {
+                            showMessageOKCancel("You need to allow access to Send SMS",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[]{Manifest.permission.SEND_SMS},
+                                                        REQUEST_SMS);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
+                        requestPermissions(new String[]{Manifest.permission.SEND_SMS},
+                                REQUEST_SMS);
+                        return;
+                    }
+                    sendFall();
+                }
+                sendFall();
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                // Set the data for the intent as the phone number.
+                callIntent.setData(Uri.parse(phoneNumber));
+                // If package resolves to an app, send intent.
+                if (callIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(callIntent);
+                } else {
+                    //Log.e(TAG, "Can't resolve app for ACTION_CALL Intent.");
+                }
                 Intent intent = new Intent(AlertWhoActivity.this, AlertSentActivity.class);
                 String message = templateNamesArr[position];
                 intent.putExtra("name", message);
                 startActivity(intent);
             }
         });
+    }
 
+    private boolean isTelephonyEnabled() {
+        if (mTelephonyManager != null) {
+            if (mTelephonyManager.getSimState() ==
+                    TelephonyManager.SIM_STATE_READY) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkForPhonePermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Permission not yet granted. Use requestPermissions().
+            //Log.d(TAG, getString(R.string.permission_not_granted));
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        } else {
+            // Permission already granted.
+        }
+    }
+
+
+    public void sendFall() {
+        //will allow text message to be sent to a contact
+        //will all be replaced once backend is connected to frontend
+        String alertMessage = " Name has fallen!";
+        Log.d("Alert Message", alertMessage);
+        String contactNumber = "8322473858";
+        //Remove whitespace and non-numeric characters
+        contactNumber = contactNumber.trim();
+        contactNumber = contactNumber.replaceAll("\\W", "");
+        if (contactNumber.length() < 10) {
+            Toast.makeText(getApplicationContext(), "Contact phone number is invalid", Toast.LENGTH_SHORT).show();
+        } else {
+
+            SmsManager sms = SmsManager.getDefault();
+            PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+            PendingIntent deliveredIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+            sms.sendTextMessage(contactNumber, null, alertMessage, sentIntent, deliveredIntent);
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.support.v7.app.AlertDialog.Builder(AlertWhoActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    public void onResume() {
+        super.onResume();
+        //setFullName(firstName, lastName);
+        sentStatusReceiver= new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                String s = "Unknown Error";
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        s = "Message Sent Successfully !!";
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        s = "Generic Failure Error";
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        s = "Error : No Service Available";
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        s = "Error : Null PDU";
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        s = "Error : Radio is off";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+        deliveredStatusReceiver=new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                String s = "Message Not Delivered";
+                switch(getResultCode()) {
+                    case Activity.RESULT_OK:
+                        s = "Message Delivered Successfully";
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                }
+            }
+        };
+        registerReceiver(sentStatusReceiver, new IntentFilter("SMS_SENT"));
+        registerReceiver(deliveredStatusReceiver, new IntentFilter("SMS_DELIVERED"));
+    }
+
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(sentStatusReceiver);
+        unregisterReceiver(deliveredStatusReceiver);
+    }
+
+    private boolean checkPermission() {
+        return ( ContextCompat.checkSelfPermission(getApplicationContext(), SEND_SMS ) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{SEND_SMS}, REQUEST_SMS);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_SMS:
+                if (grantResults.length > 0 &&  grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access sms", Toast.LENGTH_SHORT).show();
+                    sendFall();
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and sms", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(SEND_SMS)) {
+                            showMessageOKCancel("You need to allow access to both the permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[]{SEND_SMS},
+                                                        REQUEST_SMS);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
+                    }
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
