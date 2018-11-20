d2ddee342eeb9e44a250c79623b7c32f98111773
hstallworth3 <hstallworth3@gatech.edu>
2018-11-20 14:02:21 -0500

    initial hookup of pulling legitimate EC's from user to populate listview

--- mobile/src/main/java/gameofphones/gatech/stumble/UserTracker.java
+++ mobile/src/main/java/gameofphones/gatech/stumble/UserTracker.java
@@ -1,14 +1,14 @@
 package gameofphones.gatech.stumble;
 
 public class UserTracker {
-    private static final UserTracker ourInstance = new UserTracker();
+    public static final UserTracker ourInstance = new UserTracker();
     private User mUser;
 
     public static UserTracker getInstance() {
         return ourInstance;
     }
 
-    private UserTracker() {
+    public UserTracker() {
     }
 
     public User getCurrentUser() {
