4527f28ba10baf86d1b633537f67fa488092dca6
khirani6 <khirani6@users.noreply.github.com>
2018-11-23 23:47:00 -0600

    adding timer stuff from mobile

--- wear/src/main/java/gameofphones/gatech/stumble/NotificationActivity.java
+++ wear/src/main/java/gameofphones/gatech/stumble/NotificationActivity.java
@@ -2,6 +2,7 @@ package gameofphones.gatech.stumble;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.wear.widget.drawer.WearableActionDrawerView;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.drawer.WearableActionDrawer;
@@ -13,7 +14,13 @@ import android.widget.Toast;
 public class NotificationActivity extends WearableActivity implements MenuItem.OnMenuItemClickListener {
 
     private TextView mTextView;
+    private TextView textTimer;
     private WearableActionDrawerView mWearableActionDrawer;
+
+
+    private CountDownTimer countDownTimer;
+    private long timeCountInMilliSeconds = 1 * 1000;
+
 
 
 
@@ -27,12 +34,15 @@ public class NotificationActivity extends WearableActivity implements MenuItem.O
                         | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
         mTextView = (TextView) findViewById(R.id.text);
+        textTimer = findViewById(R.id.alert_text);
 
         // Enables Always-on
         setAmbientEnabled();
         mWearableActionDrawer = findViewById(R.id.bottom_action_drawer);
         mWearableActionDrawer.getController().peekDrawer();
         mWearableActionDrawer.setOnMenuItemClickListener(this);
+
+        startTimer(60);
 
     }
 
@@ -51,5 +61,33 @@ public class NotificationActivity extends WearableActivity implements MenuItem.O
         mWearableActionDrawer.getController().closeDrawer();
 
         return false;
+    }
+
+    private void startTimer(final int minuti) {
+        countDownTimer = new CountDownTimer(timeCountInMilliSeconds*minuti, 1000) {
+            // 500 means, onTick function will be called at every 500 milliseconds
+
+            @Override
+            public void onTick(long leftTimeInMilliseconds) {
+                long seconds = leftTimeInMilliseconds / 1000;
+                //barTimer.setProgress((int)seconds);
+                textTimer.setText(String.format("%02d", seconds/60) + ":" + String.format("%02d", seconds%60));
+                // format the textview to show the easily readable format
+
+            }
+            @Override
+            public void onFinish() {
+                //add functionality to automatically send alert to all ECs
+                if(textTimer.getText().equals("00:00")){
+                    //do sending alert stuff
+                    textTimer.setText("0:00");
+                }
+                else{
+                    textTimer.setText("0:10");
+                }
+            }
+        }.start();
+        countDownTimer.start();
+
     }
 }
