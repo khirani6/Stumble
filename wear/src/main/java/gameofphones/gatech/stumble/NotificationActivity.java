cef28027300364b33797c3c80da42b3986ed0c0f
khirani6 <khirani6@users.noreply.github.com>
2018-11-23 23:41:31 -0600

    adding wearable drawer to notification screen

--- wear/src/main/java/gameofphones/gatech/stumble/NotificationActivity.java
+++ wear/src/main/java/gameofphones/gatech/stumble/NotificationActivity.java
@@ -1,13 +1,21 @@
 package gameofphones.gatech.stumble;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.wear.widget.drawer.WearableActionDrawerView;
 import android.support.wearable.activity.WearableActivity;
+import android.support.wearable.view.drawer.WearableActionDrawer;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class NotificationActivity extends WearableActivity {
+public class NotificationActivity extends WearableActivity implements MenuItem.OnMenuItemClickListener {
 
     private TextView mTextView;
+    private WearableActionDrawerView mWearableActionDrawer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,26 @@ public class NotificationActivity extends WearableActivity {
 
         // Enables Always-on
         setAmbientEnabled();
+        mWearableActionDrawer = findViewById(R.id.bottom_action_drawer);
+        mWearableActionDrawer.getController().peekDrawer();
+        mWearableActionDrawer.setOnMenuItemClickListener(this);
 
+    }
 
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        final int itemId = item.getItemId();
+        switch(itemId){
+            case R.id.action_ok:
+                Intent intent = new Intent(NotificationActivity.this, HomeWearActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_not_ok:
+                //do notification stuff
+                break;
+        }
+        mWearableActionDrawer.getController().closeDrawer();
+
+        return false;
     }
 }
