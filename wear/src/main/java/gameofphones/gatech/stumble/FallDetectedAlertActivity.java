53d915067b60297a7e794014bdafb077f3ece215
khirani6 <khirani6@users.noreply.github.com>
2018-11-23 20:57:15 -0600

    Activities created
    still need to put UI stuff

---
+++ wear/src/main/java/gameofphones/gatech/stumble/FallDetectedAlertActivity.java
@@ -0,0 +1,21 @@
+package gameofphones.gatech.stumble;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class FallDetectedAlertActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fall_detected_alert);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
