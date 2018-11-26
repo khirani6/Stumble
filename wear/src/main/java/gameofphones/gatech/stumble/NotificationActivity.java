1638d97c04dffbcf2574df4a5f736fba145e041e
clawrence8 <ca.lawrence03@gmail.com>
2018-11-25 21:08:02 -0500

    Removed ScrollView and added confirmation buttons

--- wear/src/main/java/gameofphones/gatech/stumble/NotificationActivity.java
+++ wear/src/main/java/gameofphones/gatech/stumble/NotificationActivity.java
@@ -1,28 +1,16 @@
 package gameofphones.gatech.stumble;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.wear.widget.drawer.WearableActionDrawerView;
 import android.support.wearable.activity.WearableActivity;
-import android.support.wearable.view.drawer.WearableActionDrawer;
-import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class NotificationActivity extends WearableActivity implements MenuItem.OnMenuItemClickListener {
+public class NotificationActivity extends WearableActivity  {
 
-    private TextView mTextView;
     private TextView textTimer;
-    private WearableActionDrawerView mWearableActionDrawer;
-
-
     private CountDownTimer countDownTimer;
     private long timeCountInMilliSeconds = 1 * 1000;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,34 +21,12 @@ public class NotificationActivity extends WearableActivity implements MenuItem.O
                         | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
                         | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
-        mTextView = (TextView) findViewById(R.id.text);
         textTimer = findViewById(R.id.alert_text);
 
         // Enables Always-on
         setAmbientEnabled();
-        mWearableActionDrawer = findViewById(R.id.bottom_action_drawer);
-        mWearableActionDrawer.getController().peekDrawer();
-        mWearableActionDrawer.setOnMenuItemClickListener(this);
-
         startTimer(60);
 
-    }
-
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        final int itemId = item.getItemId();
-        switch(itemId){
-            case R.id.action_ok:
-                Intent intent = new Intent(NotificationActivity.this, HomeWearActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.action_not_ok:
-                //do notification stuff
-                break;
-        }
-        mWearableActionDrawer.getController().closeDrawer();
-
-        return false;
     }
 
     private void startTimer(final int minuti) {
